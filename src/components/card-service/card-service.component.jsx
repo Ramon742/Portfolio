c6a68237100a86a4e27e1f9b9498d0fd8b4e2be9
Ramon <ramondavi@msn.com>
2021-01-10 20:38:25 -0300

    some itens aligned

--- src/components/card-service/card-service.component.jsx
+++ src/components/card-service/card-service.component.jsx
@@ -7,7 +7,7 @@ const CardService = ({ img, title, description, first_point, second_point , thir
         <div className={dotted ? 'card-service card-mid' : 'card-service'}>
             <img src={img} className='img-service'  alt=""/>
             <p style={{fontSize: '24px', fontWeight: 'bold'}}>{title}</p>
-            <p style={{marginLeft: '5px', borderLeft: '4px solid #0077B6', paddingLeft: '20px', color: '#F8F8F8', textAlign: 'justify'}}>
+            <p style={{marginLeft: '5px', borderLeft: '4px solid #0077B6', paddingLeft: '20px', color: '#F8F8F8', textAlign: 'left'}}>
                 {description}
             </p>
             <ul>
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -12,21 +12,21 @@ const Footer = () => {
 
                 <div className='contact'>
                     <div style={{fontSize: '1.2rem', fontWeight: '700', marginBottom: '10px'}}>Contato</div>
-                        <div>
+                        <div style={{marginTop: '10px'}}>
                             <span style={{fontWeight: '700'}}>Telefone: </span> 
-                            <span>(84) 99702-4323</span> 
+                            <span> +55 (84) 99702-4323</span> 
                         </div>
-                        <div>
+                        <div style={{marginTop: '10px'}}>
                             <span style={{fontWeight: '700'}}>Email: </span> 
                             <span> ramondavi.dev@gmail.com </span> 
                         </div>
-                        <div>
+                        <div style={{marginTop: '10px'}}>
                             <span style={{fontWeight: '700'}}>Brazil - RN </span> 
                         </div>
                 </div>
 
                 <div className='social-network' >
-                    <div style={{fontSize: '1.2rem', fontWeight: '700', marginBottom: '10px'}} >Redes Sociais</div>
+                    <div style={{fontSize: '1.2rem', fontWeight: '700', marginBottom: '10px'}}>Redes Sociais</div>
                     <div className='links'> 
                         <a target="_blank" rel="noopener noreferrer" className='linkedin' href="https://www.linkedin.com/in/ramondavidev/"><i class="fab fa-linkedin fa-2x"></i></a> 
                         <a target="_blank" rel="noopener noreferrer" className='github' href="https://github.com/ramondavidev"><i class="fab fa-github-square fa-2x"></i></a>  
